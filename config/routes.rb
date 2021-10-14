b68d52dde1f79ad5c8a29278ea8d049a3fde1880
Ryo <test@test.com>
2021-10-14 14:33:45 +0100

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :likes
   resources :comments
   resources :posts
-  get 'home/top'
+  root 'home#top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
